7a193ce4c7f9a15de9dd31c5d2e5775c20f39ef9
Igor Bonfim <igormbonfim@hotmail.com>
2022-08-13 01:09:44 -0300

    Adicionado Contexto

---
+++ Challenge_Backend_Financas/Configuracoes/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Challenge_Backend_Financas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Challenge_Backend_Financas.Configuracoes
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Receita> Receitas { get; set; }
+        public DbSet<Despesa> Despesas { get; set; }
+    }
+}
