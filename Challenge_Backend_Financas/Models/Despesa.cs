12d2909fecade993f94ee0d70c16f66726a7bc8b
Igor Bonfim <igormbonfim@hotmail.com>
2022-08-13 01:05:03 -0300

    Models Receitas e despesas criados

---
+++ Challenge_Backend_Financas/Models/Despesa.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Challenge_Backend_Financas.Models
+{
+    [Table("TbDespesa")]
+    public class Despesa
+    {
+        [Key]
+        [Column("IdDespesa")]
+        public int Id { get; set; }
+
+        [Column("DescricaoDespesa")]
+        [Required, MaxLength(255)]
+        public string Descricao { get; set; }
+
+        [Column("ValorDespesa")]
+        [Required]
+        public double Valor { get; set; }
+
+        [Column("DataDespesa")]
+        [Required]
+        public DateOnly Data { get; set; }
+    }
+}
---
+++ Challenge_Backend_Financas/Models/Receita.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Challenge_Backend_Financas.Models
+{
+    [Table("TbReceita")]
+    public class Receita
+    {
+        [Key]
+        [Column("IdReceita")]
+        public int Id { get; set; }
+
+        [Column("DescricaoReceita")]
+        [Required, MaxLength(255)]
+        public string Descricao { get; set; }
+
+        [Column("ValorReceita")]
+        [Required]
+        public double Valor { get; set; }
+
+        [Column("DataReceita")]
+        [Required]
+        public DateOnly Data { get; set; }
+    }
+}
