619109607923b500b750b01e77c7a2bc1a180666
Igor Bonfim <igormbonfim@hotmail.com>
2022-08-18 23:35:35 -0300

    Adiciona retorno de gastos por despesa

--- Challenge_Backend_Financas/Repositories/ResumosRepository.cs
+++ Challenge_Backend_Financas/Repositories/ResumosRepository.cs
@@ -11,53 +11,86 @@ namespace Challenge_Backend_Financas.Repositories
         private readonly Contexto dbContext;
         private readonly IReceitasRepository receitasRepository;
         private readonly IDespesasRepository despesasRepository;
+        private readonly ICategoriasRepository categoriasRepository;
 
-        public ResumosRepository(Contexto dbContext, IReceitasRepository receitasRepository, IDespesasRepository despesasRepository)
+        public ResumosRepository(Contexto dbContext, IReceitasRepository receitasRepository, IDespesasRepository despesasRepository, ICategoriasRepository categoriasRepository)
         {
             this.dbContext = dbContext;
             this.receitasRepository = receitasRepository;
             this.despesasRepository = despesasRepository;
+            this.categoriasRepository = categoriasRepository;
         }
 
         public ResumoResponse GetResumoMes(int ano, int mes)
         {
-            var receitas = receitasRepository.ListByMes(ano, mes);
-            double receitasTotal = 0;
-            foreach(var r in receitas)
-            {
-                receitasTotal += r.Valor;
-            }
-
-            var despesas = despesasRepository.ListByMes(ano, mes);
-            double despesasTotal = 0;
-            foreach(var d in despesas)
-            {
-                despesasTotal += d.Valor;
-            }
+            
+            var receitasTotal = GetReceitasByMes(ano, mes);
+            var despesasTotal = GetDespesasByMes(ano, mes);
+            var gastoPorCategorias = GetGastosByCategoria(ano, mes);
 
             return new ResumoResponse()
             {
                 Mensagem = "Retorno de " + mes + "/" + ano,
                 ReceitasTotal = receitasTotal,
                 DespesasTotal = despesasTotal,
-                SaldoFinal = receitasTotal - despesasTotal
+                SaldoFinal = receitasTotal - despesasTotal,
+                GastoPorCategorias = gastoPorCategorias
+                
             };
         }
 
-        public double GetGastoPorCategoria(int ano, int mes, string categoria)
+        public List<GastoCategoriasResponse> GetGastosByCategoria(int ano, int mes)
         {
-            var receitasCategoria = dbContext.Receitas
+            var categorias = categoriasRepository.List();
+            List<GastoCategoriasResponse> gastoCategoriasResponses = new List<GastoCategoriasResponse>();
+
+            foreach (var categoria in categorias)
+            {
+                gastoCategoriasResponses.Add(GetGastoPorCategoria(ano, mes, categoria.NomeCategoria));
+            }
+            return gastoCategoriasResponses;
+        }
+
+        public double GetDespesasByMes(int ano, int mes)
+        {
+            var despesas = despesasRepository.ListByMes(ano, mes);
+            double despesasTotal = 0;
+            foreach (var d in despesas)
+            {
+                despesasTotal += d.Valor;
+            }
+            return despesasTotal;
+        }
+
+        public double GetReceitasByMes(int ano, int mes)
+        {
+            var receitas = receitasRepository.ListByMes(ano, mes);
+            double receitasTotal = 0;
+            foreach (var r in receitas)
+            {
+                receitasTotal += r.Valor;
+            }
+            return receitasTotal;
+        }
+
+        public GastoCategoriasResponse GetGastoPorCategoria(int ano, int mes, string categoria)
+        {
+            var despesasCategoria = dbContext.Despesas
                 .Include(r => r.Categoria)
                 .Where(r => r.Categoria.NomeCategoria == categoria)
                 .Where(r => r.Data.Year == ano)
                 .Where(r => r.Data.Month == mes)
                 .ToList();
             double gastoPorCategoria = 0;
-            foreach (var r in receitasCategoria)
+            foreach (var r in despesasCategoria)
             {
                 gastoPorCategoria += r.Valor;
             }
-            return gastoPorCategoria;
+            return new GastoCategoriasResponse()
+            {
+                NomeCategoria = categoria,
+                GastoTotal = gastoPorCategoria
+            };
         }
     }
 }
